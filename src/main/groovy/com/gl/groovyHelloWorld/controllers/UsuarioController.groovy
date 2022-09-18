01318cec54ae2f6b87f476e707fcda782d4ccd0c
3rick <erick.dejesus1@alumno.buap.mx>
2022-09-18 07:54:03 -0500

    add: usuario controller and usuario class

--- src/main/groovy/com/gl/groovyHelloWorld/controllers/UsuarioController.groovy
+++ src/main/groovy/com/gl/groovyHelloWorld/controllers/UsuarioController.groovy
@@ -1,12 +1,18 @@
 package com.gl.groovyHelloWorld.controllers
 
+import com.gl.groovyHelloWorld.models.Usuario
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UsuarioController {
     @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba"
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario()
+        usuario.nombre = "Erick"
+        usuario.apellido = "De Jesus"
+        usuario.email = "erick.dejesus1@alumno.buap"
+        usuario.telefono = "1238111883"
+        return usuario
     }
 }
---
+++ src/main/groovy/com/gl/groovyHelloWorld/models/Usuario.groovy
@@ -0,0 +1,9 @@
+package com.gl.groovyHelloWorld.models
+
+class Usuario {
+    String nombre
+    String apellido
+    String email
+    String telefono
+    String password
+}
